5d291a485c35b1b7bec94bffce38e25060dbc4cd
safari <rsafari.s@gmail.com>
2018-11-07 23:48:16 -0600

    updated version

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule SEnum.MixProject do
   def project do
     [
       app: :senum,
-      version: "0.1.0",
-      elixir: "~> 1.7",
+      version: "1.0.0",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
