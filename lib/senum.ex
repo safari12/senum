44d33f390a495ffc8b30d8c8e2700292d288f9fb
safari <rsafari.s@gmail.com>
2018-11-07 23:01:07 -0600

    initial commit

---
+++ lib/senum.ex
@@ -0,0 +1,18 @@
+defmodule SEnum do
+  @moduledoc """
+  Documentation for SEnum.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SEnum.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/senum_test.exs
@@ -0,0 +1,8 @@
+defmodule SEnumTest do
+  use ExUnit.Case
+  doctest SEnum
+
+  test "greets the world" do
+    assert SEnum.hello() == :world
+  end
+end
